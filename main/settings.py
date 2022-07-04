613772d1eba3fd21de48460cacca5a78b57c859c
Katia Benassil <37975517+katwod@users.noreply.github.com>
2022-07-04 13:01:50 +0100

    updated media url

--- main/settings.py
+++ main/settings.py
@@ -146,7 +146,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-#MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
